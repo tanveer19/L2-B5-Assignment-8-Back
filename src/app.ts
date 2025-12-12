4279225f5d5441bdca72b7199cd5246690dd785f
tanveer19 <thjbd19@gmail.com>
2025-12-12 22:22:32 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://l2-b5-assignment-8.vercel.app"],
     credentials: true,
   })
 );
